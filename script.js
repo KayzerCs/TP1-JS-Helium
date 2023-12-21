5589516223da11b909d2ac0d2fc4365ee2685a02
Kayzer <max.mlv2210@gmail.com>
2023-12-21 21:23:51 +0100

    Correction terminé

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
-
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
-
-// S'assurer que les liens sont clickables
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
-
-const mousemove = document.querySelector(".mousemove");
-const mousemove2 = document.querySelector(".mousemove2");
-const mousemove3 = document.querySelector(".mousemove3");
+const mouses = document.querySelectorAll(".mouse");
 
 window.addEventListener("mousemove", (e) => {
-  mousemove.style.left = e.pageX + "px";
-  mousemove.style.top = e.pageY + "px";
-
-  mousemove2.style.left = e.pageX + "px";
-  mousemove2.style.top = e.pageY + "px";
-
-  mousemove3.style.left = e.pageX + "px";
-  mousemove3.style.top = e.pageY + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
 });
 
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
+
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
+
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
+// });
